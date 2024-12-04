5077db865e8334316ace0ff8a02c9f8118d03426
unknown <fernandobern.contato@gmail.com>
2024-12-03 22:31:12 -0300

    Ajustes finais na tela inicial

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -40,4 +40,4 @@ export default function Login() {
             </form>
         </div>
     )
-}
+};
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="home-main">
+        <h1>Bem-vindo à Home!</h1>
+        <p>Esta é a página inicial.</p>
+      </div>
+    );
+  }
+  
